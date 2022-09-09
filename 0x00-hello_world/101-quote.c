99d197200cc0f49ba22d4cf58a68783db13442e2
Amgboji <uplaf@yahoo.com>
2022-09-08 23:27:26 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,5 @@
  #include <stdio.h>
 
-
-
  /** 
 
  * main - Entry point  
@@ -15,4 +13,6 @@
  {         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61); 
 
         return (1); }
+
+
 
