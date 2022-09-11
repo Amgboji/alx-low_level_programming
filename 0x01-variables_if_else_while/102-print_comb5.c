fdfc4ce6ff68e882f4a9db682cc0f26646047960
Amgboji <uplaf@yahoo.com>
2022-09-11 14:48:17 -0700

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,8 +10,6 @@ int main(void)
 {
 	int i;
 	int j;
-	
-
 	for (i = 0; i <= 99; i++)
 
 {
@@ -23,7 +21,7 @@ int main(void)
 	putchar(j / 10 + '0');
 	putchar(j % 10 + '0'); 
 	if (!(i == 98 && j == 99))
-		{
+{
 			putchar(',');
 			putchar(' ');
 		}
