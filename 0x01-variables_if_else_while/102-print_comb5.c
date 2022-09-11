de65d9af6b88edd06617b71f1861a25843310bff
Amgboji <uplaf@yahoo.com>
2022-09-11 10:28:59 -0700

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,26 @@
 
 #include <stdio.h>
 /**
- * main - program that prints all possible different combinations of 2 digits.
+ * main - program that prints all possible different combinations of 2 digits
  * Return: 0
  */
 int main(void)
 {
 	int c = 0;
 	int f_d;
-	int m_d;
 	int l_d;
-   
-	while (c <= 00)
+
+	while (c <= 99)
 	{
-		f_d = (c / 01 + '0');
-		m_d = (c / 10 % 10 + '0');
+		f_d = (c / 10 + '0');
 		l_d = (c % 10 + '0');
 
-		if ((f_d < m_d) && (m_d < l_d))
+		if (f_d < l_d)
 		{
 			putchar(f_d);
-			putchar(m_d);
 			putchar(l_d);
 
-			if (c != 99)
+			if (c != 89)
 			{
 				putchar(',');
 				putchar(' ');
@@ -31,6 +28,6 @@ int main(void)
 		}
 		c++;
 	}
-put-char('\n');
+putchar('\n');
 return (0);
-}
+}[B[B
