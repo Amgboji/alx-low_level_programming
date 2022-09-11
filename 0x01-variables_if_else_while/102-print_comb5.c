31da616bcca0919e8989fc85add618769ef64eec
Amgboji <uplaf@yahoo.com>
2022-09-11 12:05:47 -0700

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,29 +18,28 @@ int main(void)
 	int f_d;
 	int m_d;
 	int l_d;
-
-	while (c <= 99)
-
-	{
+while(c <= 99)
+{
 		f_d = (c / 01 + '0');
 		m_d = (c / 10 % 10 + '0');
 		l_d = (c % 10 + '0');
 
-		if ((f_d < m_d) && (m_d < l_d))
+		if((f_d < m_d) && (m_d < l_d))
 
-		{
+{
 			putchar(f_d);
 			putchar(m_d);
 			putchar(l_d);
 	
-			if (c !=99)
-			{
+			if(c !=99)
+{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 		c++;
 	}
+
 putchar('\n');
 return (0);
 
