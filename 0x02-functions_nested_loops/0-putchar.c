aa05633033bdb59f0f72d7e52df8cab2d2c9da93
Amgboji <uplaf@yahoo.com>
2022-09-13 06:01:48 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that return 
+ *
+ * Descriptio: Program that print Putchar, follow by new line 
+ */
+
+int main ()
+
+{
+	Putchar("print Putchar");
+	Putchar('\n');
+
+	return 0;
+
+
+	}
+
+
+	
