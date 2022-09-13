aec8c98f4b0aa71433c724f51fb2cae69d9cc84d
Amgboji <uplaf@yahoo.com>
2022-09-13 08:03:19 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,7 +13,13 @@ int main()
 
 {
 	putchar('_');
-	putchar('putchar');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	putchar('\n');
 
 	return 0;
