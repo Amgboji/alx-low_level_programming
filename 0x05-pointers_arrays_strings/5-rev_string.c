edcddfe2edcc1a7d1d0546c472125b4d044de17f
Amgboji <uplaf@yahoo.com>
2022-09-19 06:51:52 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,24 @@
-#include "main.h"
-#include <stdio.h>
+#include "main.h"                                                                                                   
+                                                                                                                    
+/**                                                                                                                 
+ *  * main - rev_string - reverse the string.                                                                          
+ *   *                                                                                                                  
+ *    *Description: pointer to strings.                                                                                  
+ *     * Return: void                                                                                                     
+ *      */                                                                                                                 
+void rev_string(char *s);                                                                        
+int i, j, k, temp;                                                                                          
+                                                                                                                    
+i=0                                                                                                                 
+while (s[i] ! = "\0)                                                                                                
+                                                                                                                    
+{                                                                                             i++;                                                           
+}                                                                                                                                                       
+k=0;                                                                            j=i-1;                                                                                                              
+while (k<j)                                                                                            	                                                         {                     
+	        temp = s[k]                                                                     s[k] = s[j]                                                                     s[j] = temp;
+		k++;
+		j--;
+}
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s[10] = "My School";
-
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
 }
