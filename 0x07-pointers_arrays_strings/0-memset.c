a1c4931f1edfd8864359a4f311141fc588581bb2
Amgboji <uplaf@yahoo.com>
2022-09-26 14:16:08 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,11 +12,10 @@
 char *_memset(char *s, char b, unsigned int n);
 
 {
-	while (n)
+unsigned int a;
 
-{
-	s[n - 1] =b;
-	n--;
+  for (a =0; a < n; a++)
+	  s[a] =b;
 	
+	return (s);
 }
-return (s);
