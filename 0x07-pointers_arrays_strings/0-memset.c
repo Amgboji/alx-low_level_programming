2dc9a9778d6d2d2954ea1b2ec63bf743ccfc1dcb
Amgboji <uplaf@yahoo.com>
2022-09-26 12:07:33 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
 #include <main.h>
 
 /**
-*_memset -fill a memory with constant bite
+* _memset -fill a memory with constant byte
 * @s: Address to memory block
-* @b:char to be used 
-* @n: number of bite to be used[D[D
+* @b: char to be used 
+* @n: number of byte to be used
 *
 * return: pointer to the memory block
 */
 
 char *_memset(char *s, char b, unsigned int n);
+
 {
-while (n)
+	while (n)
 
 {
 	s[n - 1] =b;
 	n--;
 }
-return: (s);
+return (s);
