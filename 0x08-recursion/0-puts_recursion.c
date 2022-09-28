20a624236d0351affea441576d9050dd2adfbd91
Amgboji <uplaf@yahoo.com>
2022-09-28 02:21:23 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
 void _puts_recursion(char *s)
 
 {
-    if (*s)
+    if(*s)
     {
-        _putchar(*s);
-        _puts_recursion(s + 1);
+      _putchar(*s);
+      _puts_recursion(s + 1);
     }
-    if (!*s)
+    if(!*s)
     _putchar('\n');
 }
