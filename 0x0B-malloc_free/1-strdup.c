f57e9db37b9685ebfc70cb58efcef04159da2297
Amgboji <uplaf@yahoo.com>
2022-10-04 15:26:23 -0700

    task01mollac

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,41 +1,27 @@
 #include "main.h"
 
 /**
-* str_concat -> string concatinating function
-* @s1: string 1
-* @s2: string 2
-* Return: string 1 + string 2
+* _strdup -> string duplicator function
+* @str: string to be dupliated
+* Return: a string pointer
 */
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	int i = 0, j = 0, l = 0, k = 0;
+	int i = 1, j = 0;
 	char *s;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-
-	l = i + j;
-	s = (char *)malloc(l * sizeof(char) + 1);
-	if (s == NULL)
+	if (str == NULL)
 		return (NULL);
-	j = 0;
-	while (k < l)
+	while (str[i])
+		i++;
+	s = (char *)malloc(i * sizeof(char) + 1);
+if (s == NULL)
+		return (NULL);
+	while (j < i)
 	{
-		if (k < i)
-			s[k] = s1[k];
-		if (k >= i)
-		{
-			s[k] = s2[j];
-			j++;
-		}
-		k++;
+		s[j] = str[j];
+		j++;
 	}
-	s[k] = '\0';
+	s[j] = '\0';
 	return (s);
 }
