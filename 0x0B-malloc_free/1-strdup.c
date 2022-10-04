86dc7d90c23da33fe7735e8c0c38893562da5c57
Amgboji <uplaf@yahoo.com>
2022-10-04 15:20:23 -0700

    task01mollac

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * create_array -> creating array
 * @size: size of array to be created
@@ -7,14 +8,16 @@
 */
 char *create_array(unsigned int size, char c)
 {
-unsigned int i;
-char *s;
-if (size == 0)
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
 return (NULL);
-s = (char *)malloc(size * sizeof(char));
-if (s == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
-s[i] = c;
-return (s);
+	s = (char *)malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
 }
